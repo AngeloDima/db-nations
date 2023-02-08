c1428b402a72a58a262381ff3a42fa848e6f5a11
AngeloDima <Angelodimauro91@gmail.com>
2023-02-08 15:35:30 +0100

    Milestone 3 (Filtro)

--- src/NationJavaDb/NationJavaDb.java
+++ src/NationJavaDb/NationJavaDb.java
@@ -15,48 +15,52 @@ public class NationJavaDb {
 		String user = "root";
 		String password ="root";
 		
-		Scanner data = new Scanner(System.in);
 		
-		
+		Scanner scan = new Scanner(System.in);
+
 		Connection con = null;
 		try {
-			con = DriverManager.getConnection(url, user, password);
-			  String sql="SELECT countries.country_id  as id_paese ,countries.name as nome_paese, regions.name as nome_regione, continents.name as nome_continente\n"
-			  		+ "FROM countries \n"
-			  		+ "Inner join regions \n"
-			  		+ "on countries.region_id  = regions.region_id  \n"
-			  		+ "Inner join continents \n"
-			  		+ "on regions.continent_id = continents.continent_id \n"
-			  		+ "Order by countries.name ";
-			  
-			  try(PreparedStatement ps=con.prepareStatement(sql)) {
-
-			  //qui posso utilizzare il PreparedStatement
-				  
-			 try(ResultSet rs =ps.executeQuery() )  {
-			  //qui posso utilizzare il ResultSet
-				 
-				 System.out.println("Id nazione\t\tnazione\t\t\t\t\t\t\tregione\t\t\t\t\t\t\t\t\t\t\t\tContinente");
-				  while (rs.next()) { 
-					  System.out.println(
-							  rs.getString(1) +"\t\t\t"+
-							  rs.getString(2) + "\t\t\t\t\t\t\t" +
-							  rs.getString(3)+ "\t\t\t\t\t\t\t\t\t\t\t" + 
-							  rs.getString(4));
-				  		}
-				  }
-			 }
-			  
-			 
-
-		}catch(SQLException ex) {
-			ex.printStackTrace();
-		}finally {
-			if(con != null) {
-				//ricordarsi di chiudere la connessione quando si finisce
-				con.close();
-			}
+		    con = DriverManager.getConnection(url, user, password);
+		    System.out.print("Inserisci il nome di una città: ");
+		    
+		    String city = scan.nextLine();
+		    
+		    System.out.println("");
+		    
+		    String sql = "SELECT countries.country_id as id_paese, countries.name as nome_paese, regions.name as nome_regione, continents.name as nome_continente\n"
+		            + "FROM countries\n"
+		            + "Inner join regions\n"
+		            + "on countries.region_id = regions.region_id\n"
+		            + "Inner join continents\n"
+		            + "on regions.continent_id = continents.continent_id\n"
+		            + "Where countries.name = ?\n"
+		            + "Order by countries.name";
+		    
+		    try (PreparedStatement ps = con.prepareStatement(sql)) {
+		        ps.setString(1, city);
+		        
+		        try (ResultSet rs = ps.executeQuery()) {
+		            System.out.println("Id nazione\t\tnazione\t\t\t\t\t\t\tregione\t\t\t\t\t\t\t\t\t\t\t\tContinente");
+		            if (rs.next()) {
+		                System.out.println(
+		                        rs.getString(1) + "\t\t\t" +
+		                        rs.getString(2) + "\t\t\t\t\t\t\t" +
+		                        rs.getString(3) + "\t\t\t\t\t\t\t\t\t\t\t" +
+		                        rs.getString(4));
+		            } else {
+		                System.out.println("Nessuna città trovata con il nome " + city);
+		            }
+		        }
+		    }
+		} catch (SQLException ex) {
+		    ex.printStackTrace();
+		} finally {
+		    if (con != null) {
+		        con.close();
+		    }
 		}
+		
+		
 
 	}
 
